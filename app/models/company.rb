5cc1abc28f5f6a34c517948113af3898a464d4a8
Shubham Sahu <shubham.s@lucideustech.com>
2021-01-17 13:23:15 +0530

    Task #3: Show city and state when zip code is updated

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,21 @@ class Company < ApplicationRecord
         format: { with: /.+@getmainstreet\.com\z/,
                   message: "We currently accept only getmainstreet.com email domains" },
         if: -> { email_changed? && email? }
+
+  before_save :update_city_state, if: :zip_code_changed?
+
+  def city_state
+    [city, state].compact.blank? ? nil : [city, state].join(', ')
+  end
+
+  private
+
+  def update_city_state
+    address = ZipCodes.identify(zip_code)
+    unless address.nil?
+      self.city = address[:city]
+      self.state = address[:state_code]
+    end
+  end
+
 end
