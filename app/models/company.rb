cffa9d8b3c94050b29e17df0f870f42c66422d95
Shubham Sahu <shubham.s@lucideustech.com>
2021-01-17 13:18:54 +0530

    Task #2: Email validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ApplicationRecord
   has_rich_text :description
 
-
+  validates :email,
+        format: { with: /.+@getmainstreet\.com\z/,
+                  message: "We currently accept only getmainstreet.com email domains" },
+        if: -> { email_changed? && email? }
 end
